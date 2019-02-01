cd2563ae7d5d7d902e74feed2dce514f5c70423b
Federico Viscomi <federico.viscomi@dh.com>
2019-02-01 15:01:51 +0000

    add windows fix

--- section_16.commands.sh
+++ section_16.commands.sh
@@ -9,9 +9,9 @@ MSYS_NO_PATHCONV=1 winpty docker container run -it -v ${pwd}:/app ruby:2.3 sh
 gem install travis --no-rdoc --no-ri
 gem install travis
 travis 
-travis login # use github credentials
+travis login --pro # use github credentials
 
 
-travis encrypt-file service-account.json -r federicoviscomi/multi-k8s
+travis encrypt-file service-account.json --pro -r federicoviscomi/multi-k8s
 
 
