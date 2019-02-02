516d80dcb0b38fa13755c00c152ba038bd4d3d86
federicoviscomi <f@neterea.com>
2019-02-02 22:19:30 +0000

    16.244

--- create_secret.sh
+++ create_secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # WARNING: plain text password!
-kubectl create secret generic pgpassword --from-literal PGPASSWORD=12345asdf
+kubectl create secret generic pgpassword --from-literal PGPASSWORD=mypgpassword123
